0a020cd89322a3ae5f140a52c6b961ea7ff3030e
Tom Sherman <the.tomsherman@gmail.com>
2024-01-04 17:05:58 +0000

    Report duraction when storing artifacts in remote-nx

--- packages/remote-nx/src/setup-sdk.ts
+++ packages/remote-nx/src/setup-sdk.ts
@@ -1,19 +1,38 @@
+import type { Task } from 'nx/src/config/task-graph';
 import { initEnv } from 'nx-remotecache-custom';
 import { getVercelRemoteCacheClient } from './remote-client';
 import type { Readable } from 'stream';
 import type { VercelRemoteCacheOptions } from './remote-client';
 
 // eslint-disable-next-line @typescript-eslint/require-await
-const setupSDK = async (options: VercelRemoteCacheOptions) => {
+const setupSDK = async (options: VercelRemoteCacheOptions, tasks: Task[]) => {
   initEnv(options);
   const remote = getVercelRemoteCacheClient(options);
+
   return {
     name: 'Vercel Remote Cache',
     fileExists: (filename: string) => remote.exists(filename).send(),
     retrieveFile: (filename: string) => remote.get(filename).stream(),
     storeFile: (filename: string, stream: Readable) =>
-      remote.put(filename).stream(stream),
+      remote
+        .put(filename, {
+          duration: totalDuration(tasks),
+        })
+        .stream(stream),
   };
 };
+
+function totalDuration(tasks: Pick<Task, 'endTime' | 'startTime'>[]) {
+  let total = 0;
+  for (const task of tasks) {
+    if (task.startTime && task.endTime) {
+      total += task.endTime - task.startTime;
+    }
+  }
+
+  console.log('total duration', total);
+
+  return total;
+}
 
 export { setupSDK };
